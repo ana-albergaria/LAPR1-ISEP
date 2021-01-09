fbd91ca74735392c2ddd37fade30e4feac00afba
JLWolff <joaolwolff@gmail.com>
2021-01-09 12:27:45 +0000

    adicionando método de leitura de ficheiros ao main

--- src/main.java
+++ src/main.java
@@ -1,24 +1,127 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class main{
+    static Scanner read = new Scanner(System.in);
     public static void main(String[] args) throws IOException, InterruptedException {
-        populationDistribution();
+        String fileName = read.next();
+        String[] speciesName = speciesName(fileName);
+        int size = sizeMatrix(fileName);
+        double [] sizeSpecies = new double[size];
+        double[][] leslieMatrix = new double[size][size];
+        readFile(fileName, sizeSpecies, leslieMatrix);
+        //print(sizeSpecies);
+        //print1(leslieMatrix);
+        populationDistribution(sizeSpecies, leslieMatrix);
         callGnuplot();
 
     }
-    public static void populationDistribution () throws IOException {
+    public static String[] speciesName (String path){
+        String[] specie = path.split(".txt");
+        return specie;
+    }
+    public static void readFile (String path, double[] size, double[][] leslie) throws FileNotFoundException {
+        String vector;
+        String [] auxVector;
+        int cont=0, i;
+        File archive = new File(path);
+        Scanner readFile = new Scanner(archive);
+
+        /*
+        String [] auxVector2;
+        String [] auxVector3;
+        String vetor1 = readFile.nextLine();
+        auxVector= transformVector(vetor1);
+        String vetor2 = readFile.nextLine();
+        auxVector2 = transformVector(vetor2);
+        String vetor3 = readFile.nextLine();
+        auxVector3 = transformVector(vetor3);
+        for (int m=1; m<auxVector.length; m++){
+            for (int k=0; k<auxVector.length; k++){
+                if (m == k+1){
+                    leslie[m][k] = Double.parseDouble(auxVector2[k]);
+                }
+            }
+        }
+        for (int j=0; j<auxVector.length; j++){
+            leslie[0][j] = Double.parseDouble(auxVector3[j]);
+        }
+        */
+
+        do{
+            vector = readFile.nextLine();
+            auxVector = transformVector(vector);
+            switch (cont){
+                case 0:
+                    for (i=0; i<auxVector.length; i++){
+                        size[i] = Integer.parseInt(auxVector[i]);
+                    }
+                    break;
+                case 1:
+                    for (int m=1; m<leslie.length; m++) {
+                        for (int k = 0; k < leslie.length; k++) {
+                            if (m == k + 1) {
+                                leslie[m][k] = Double.parseDouble(auxVector[k]);
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    for (i=0; i<auxVector.length; i++){
+                        leslie[0][i] = Double.parseDouble(auxVector[i]);
+                    }
+                    break;
+            }
+            cont++;
+        }while (readFile.hasNextLine());
+
+        readFile.close();
+    }
+
+
+
+    public static String[] transformVector (String vector){
+        String[] auxVector = vector.split(", ");
+        for (int i=0; i<auxVector.length; i++){
+            auxVector[i] = auxVector[i].substring(auxVector[i].indexOf("=")+1);
+        }
+        return auxVector;
+    }
+
+    public static int sizeMatrix (String path) throws FileNotFoundException {
+        File archive = new File(path);
+        Scanner readFile = new Scanner(archive);
+        String vector = readFile.nextLine();
+        int size = transformVector(vector).length;
+        readFile.close();
+        return size;
+    }
+
+    public static void print (int[] size){
+        for (int i=0; i< size.length; i++){
+            System.out.printf("%d ", size[i]);
+            System.out.println();
+        }
+    }
+
+    public static void print1 (double[][] size){
+        for (int i=0; i< size.length; i++){
+            for (int k=0; k< size.length; k++){
+                System.out.printf("%f ", size[i][k]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void populationDistribution (double [] initialPopVec, double[][] leslieMatrix) throws IOException {
         Scanner read = new Scanner(System.in);
 
-        double initialPopVec[] = {1000, 300, 330, 100};
+        //double initialPopVec[] = {1000, 300, 330, 100};
         //double[][] leslieMatrix = {{0.50,2.40,1,0},{0.5,0,0,0},{0,0.8,0,0},{0,0,0.5,0}};
-        double[][] leslieMatrix = {{0, 3, 3.17, 0.39}, {0.11, 0, 0, 0}, {0, 0.29, 0, 0}, {0, 0, 0.33, 0}};
+        //double[][] leslieMatrix = {{0, 3, 3.17, 0.39}, {0.11, 0, 0, 0}, {0, 0.29, 0, 0}, {0, 0, 0.33, 0}};
 
         printPopDistribution(initialPopVec);
         printMatrix(leslieMatrix);
