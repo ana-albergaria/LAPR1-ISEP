76c58cfdb15f598a4c8db56a45639fd8b4be7de9
manuela_leite <1200720@isep.ipp.pt>
2021-01-11 10:04:46 +0000

    método de tratamento dos dados vindos do terminal

---
+++ src/tratamentOfDatasTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class tratamentOfDatasTerminal {
+    static Scanner read = new Scanner(System.in);
+    public static void main(String[] args) {
+        switch (args.length){
+            case 0: case 2:
+                //ask the user the necessary questions for iteractive modes
+                //isert the necessary method
+                //case 2 split the array after -n to take the arquiche name
+                break;
+            case 9:
+                //no interactive mode with all the features
+                break;
+            case 4:
+                //no interactive mode without any features
+                break;
+            case 7: case 8:
+                //check how features the user want
+                // -e augevalues and augevectors
+                // -v dimension of population
+                // -r variation of population in each generation  
+
+
+        }
+    }
+}
